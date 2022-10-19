840476f213f5888d8c0dfb18e1543279041bef02
Niemir Wisniewski <niemirwisniewski@gmail.com>
2022-10-19 12:12:20 +0200

    surowy stan aplikacji

---
+++ src/main/java/pl/niemirwisniewski/hehexd/HehexdApplication.java
@@ -0,0 +1,13 @@
+package pl.niemirwisniewski.hehexd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HehexdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HehexdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/niemirwisniewski/hehexd/HehexdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.niemirwisniewski.hehexd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HehexdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
